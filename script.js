f8dc8be48dc0bdc2644abf64b3c20e7fe97ed09b
Tylre Collins <tyler@nhlp.com>
2020-04-08 10:26:53 -0600

    inital object made and changed append target of function

--- script.js
+++ script.js
@@ -84,22 +84,36 @@ nextQuestion()
 // need to add question variable
 // Need to add more specific picture choices in queryURL
 function displayPlanetInfo(){
-    var planetName = 'whatever the questions feeds in'
-    var queryURL = 'https://images-api.nasa.gov/search?q=saturn'
-    console.log(queryURL)
+    var planetName = 'saturn'
+    var queryURL = 'https://images-api.nasa.gov/search?q=' + planetName
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(response)
+        $('.content').empty()
         var planetImage = $('<img>')
-        planetImage.attr('src', response.collection.items[0].links[0].href)
-        $('body').append(planetImage)
         var planetDescription =$('<p>')
+        var funnyDescription = $('<p>')
+        planetImage.attr('src', response.collection.items[0].links[0].href)
         planetDescription.text(response.collection.items[0].data[0].description)
-        $('body').append(planetDescription)
+        $('.content').append(planetImage)
+        $('.content').append(planetDescription)
+        $('.content').append(funnyDescription)
    
 })
 }
-// displayPlanetInfo()
+displayPlanetInfo()
 
+var clickBaitDescription = {
+  mercury: "",
+  venus: "",
+  earth: "",
+  mars: "",
+  jupiter: "",
+  saturn: "",
+  uranus: "",
+  neptune:"",
+  pluto: "",
+
+}
