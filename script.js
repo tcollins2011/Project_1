ae7d153be98ef37a30e1b2332abc981d7dfea443
Tao <tao8261@aol.com>
2020-04-08 10:25:42 -0600

    changes to js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
    
-   // Questions in Object Array
 
 
 var questions = [
@@ -27,10 +26,16 @@ var questions = [
           },
     ]
 
-$(document).ready(function() {
-// $("#start").on('click', function to go here
+// $(document).ready(function() {
+      $("#startBtn").on('click', function(){
+            $(".content").empty()
+            startQuiz()
+      })
+      
+      
+      
+     
 
-})
 
 var questionnaire = {
       currentQuestion: 0,
@@ -46,30 +51,32 @@ var questionnaire = {
             q2: ['Dog', 'Cat'],
             q3: ['Mashed', 'Baked'],
           },
-      answers: {
-            q1: 'Batman',
-            q2: 'Dog',
-            q3: 'Mashed',
-          },
+    
 
       startQuiz: function(){
+            
+           
             questionnaire.currentQuestion = 0
             questionnaire.score = 0
+            
 
             questionnaire.nextQuestion()
-
+            
       },
+      nextQuestion: function () {
+           
+            var questionContent = Object.values(questionnaire.questions)[questionnaire.currentQuestion]
+            var questionOptions = Object.values(questionnaire.answers)[questionnaire.currentQuestion]
+            
+            $(".qustionBody").text(questionContent);
+            $("#test").text(questionOptions);
+            
+      }
 
+      
 // forloop for Q's
-
+// using displayQuestion, displayOptions, etc
 }
-function nextQuestion () {
-      var questionContent = Object.values(questionnaire.questions)[questionnaire.currentQuestion]
-      
-      $("#test").text(questionContent);
-      
-}
-nextQuestion()
 
 
 
@@ -81,25 +88,27 @@ nextQuestion()
 
 
 
-// need to add question variable
-// Need to add more specific picture choices in queryURL
-function displayPlanetInfo(){
-    var planetName = 'whatever the questions feeds in'
-    var queryURL = 'https://images-api.nasa.gov/search?q=saturn'
-    console.log(queryURL)
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response)
-        var planetImage = $('<img>')
-        planetImage.attr('src', response.collection.items[0].links[0].href)
-        $('body').append(planetImage)
-        var planetDescription =$('<p>')
-        planetDescription.text(response.collection.items[0].data[0].description)
-        $('body').append(planetDescription)
+// // THIS IS TYLER'S CODE BELOW
+
+// // need to add question variable
+// // Need to add more specific picture choices in queryURL
+// function displayPlanetInfo(){
+//     var planetName = 'whatever the questions feeds in'
+//     var queryURL = 'https://images-api.nasa.gov/search?q=saturn'
+//     console.log(queryURL)
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response){
+//         console.log(response)
+//         var planetImage = $('<img>')
+//         planetImage.attr('src', response.collection.items[0].links[0].href)
+//         $('body').append(planetImage)
+//         var planetDescription =$('<p>')
+//         planetDescription.text(response.collection.items[0].data[0].description)
+//         $('body').append(planetDescription)
    
-})
-}
-displayPlanetInfo()
+// })
+// }
+// displayPlanetInfo()
 
