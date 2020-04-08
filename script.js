9c670fc01841aa7eb21d6f8564e8478386aaed8b
Tao <tao8261@aol.com>
2020-04-08 13:27:17 -0600

    question cycle working

--- script.js
+++ script.js
@@ -74,8 +74,7 @@ var questionnaire = {
       
             $(".button").on("click", function() {
                   $(".content").empty()
-                  
-                  
+                  secondQuestion()
                   
             })
            
@@ -126,8 +125,7 @@ var questionnaire = {
       
             $(".button").on("click", function() {
                   $(".content").empty()
-
-                  
+                  fourthQuestion()
                   
             })
            
@@ -175,6 +173,11 @@ console.log(currentQuestion)
 
 
 
+
+
+
+
+// TYLERS CODE
 var clickBaitDescription = {
       'mercury': "",
       'venus': " Something witty and funny that will be written later",
@@ -195,33 +198,6 @@ var clickBaitDescription = {
 
 
 
-
-
-// // THIS IS TYLER'S CODE BELOW
-
-// // need to add question variable
-// // Need to add more specific picture choices in queryURL
-// function displayPlanetInfo(){
-//     var planetName = 'whatever the questions feeds in'
-//     var queryURL = 'https://images-api.nasa.gov/search?q=saturn'
-//     console.log(queryURL)
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function(response){
-//         console.log(response)
-//         var planetImage = $('<img>')
-//         planetImage.attr('src', response.collection.items[0].links[0].href)
-//         $('body').append(planetImage)
-//         var planetDescription =$('<p>')
-//         planetDescription.text(response.collection.items[0].data[0].description)
-//         $('body').append(planetDescription)
-   
-
-// displayPlanetInfo()
-
-// need to add question variable
-// Need to add more specific picture choices in queryURL
 function displayPlanetInfo(){
     // var planetName = 'saturn'
     var queryURL = 'https://images-api.nasa.gov/search?q=' + planetName
